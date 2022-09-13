93f49334f1c658ae3cd7a4ab48b5236b27376d5c
Alexandre Barth <alebarth2001@gmail.com>
2022-09-12 21:56:56 -0300

    JavaScript - Manipulação da DOM

---
+++ aula001/index.js
@@ -0,0 +1,6 @@
+// selecionando o elemento que vai ser alterado do html
+const text = document.querySelector('p');
+
+// alterando os dados com innerHTML
+text.innerHTML = 'Olá mundo'
+text.style.color = '#f33'
